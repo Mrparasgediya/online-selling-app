df1674e7de34d4e6405c25c2743ac28c458b77e1
Mrparasgediya <mrparas103@gmail.com>
2022-04-09 14:47:35 +0530

    completed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  env: {},
+  env: {
+    APP_BASE_URL: "", // your base app url name
+    JWT_SECRET: "", // jwt secret
+    MONGODB_URI: "", //mongodb connection url
+    CRYPTO_SECRET: "", // crypto secret
+  },
 };
 
 module.exports = nextConfig;
